fd0e83e9dbb5640e5752ab5b0e8721dd35b6314e
Manujaya94 <34622276+Manujaya94@users.noreply.github.com>
2018-09-03 11:22:23 +0530

    Add files via upload

---
+++ getDataServlet.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class getDataServlet extends HttpServlet {
+    public getDataServlet(){
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        //*get parameters
+    }
+
+}
